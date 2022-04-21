ccef01a948617b23f318dbe7fd864a3910694a81
annniiieee <100371639+annniiieee@users.noreply.github.com>
2022-04-21 13:53:16 -0400

    Create iAdminRights.java

---
+++ iAdminRights.java
@@ -0,0 +1,13 @@
+package termproject;
+
+
+public interface iAdminRights {
+    void addBooks(); 
+    void removeBooks(); 
+    void addUsers(); 
+    void removeUsers(); 
+    void viewUsers(); 
+    void viewLoans(); 
+    void changeUserPassword(); 
+    
+}
