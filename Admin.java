39ea6b54b8289b9afec6796c086907578773ea2d
annniiieee <100371639+annniiieee@users.noreply.github.com>
2022-04-20 23:59:23 -0400

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,14 +1,69 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package library;
-
-/**
- *
- * @author Admin
- */
 public class Admin {
+    protected int id; 
+    protected String password;
+    protected String name; 
+    protected String address; 
+    static int counter; 
+    protected double salary; 
+
+    public admin(int id, String password, String name, String address, double salary) {
+        counter++; 
+        this.id = counter;
+        this.password = password;
+        this.name = name;
+        this.address = address;
+        this.salary = salary; 
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+ 
+    
+    
+}
+
     
 }
