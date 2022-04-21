f418461ba18ad49b546dc7bbab0008fcd6507389
Msrd1 <104034719+Msrd1@users.noreply.github.com>
2022-04-21 11:36:01 -0400

    Update UserInputManager.java

--- UserInputManager.java
+++ UserInputManager.java
@@ -26,7 +26,7 @@ public class UserInputManager {
     
     public String getPassWord(){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Password: ");
+        System.out.print("PLease input password: ");
         return sc.nextLine();        
     }
     
@@ -45,7 +45,7 @@ public class UserInputManager {
     
     public String getFirstName(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please input your first name.");
+        System.out.print("Please input your first name. ");
         String fName = sc.nextLine();
         while (fName.equals("")) {            
             System.out.println("Empty name. Please input a name.");
@@ -56,7 +56,7 @@ public class UserInputManager {
 
     public String getLastName(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please input your last name.");
+        System.out.print("Please input your last name. ");
         String lName = sc.nextLine();
         while (lName.equals("")) {            
             System.out.println("Empty name. Please input a name.");
@@ -67,7 +67,7 @@ public class UserInputManager {
     
     public String getAddress(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please input your address.");
+        System.out.print("Please input your address. ");
         String address = sc.nextLine();
         while (address.equals("")) {            
             System.out.println("Empty address. Please input a address.");
@@ -86,4 +86,46 @@ public class UserInputManager {
         }
         return password;          
     }
+    
+    public String getNewPassword(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your new password?");
+        String newS = sc.nextLine();
+        System.out.println("Please enter your new password again."); 
+        String newCheckS = sc.nextLine();
+        if (newS.equals(newCheckS)) {
+            return newS;
+        }
+        else{
+            System.out.println("Entered passwords do not match.");
+            return getNewPassword();
+        }
+    }
+    
+    public int displayUserOption(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to library inc.! What would you like to do today? "
+                         + "\n1. Create a new user." 
+                         + "\n2. Change a password." 
+                         + "\n3. Borrow a book." 
+                         + "\n4. Return a book."
+                         + "\n5. Search a book."
+                         + "\n6. View and pay fees.");
+        return sc.nextInt();
+    }
+    
+    public int displayAdminOption(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to library inc.! What would you like to do today? "
+                         + "\n1. Create a new admin account." 
+                         + "\n2. Change an admin password." 
+                         + "\n3. Search a book." 
+                         + "\n4. Add a book."
+                         + "\n5. Delete a book."
+                         + "\n6. Search a user's information."
+                         + "\n7. Delete a user."
+                         + "\n8. View a user's loans and fees.");        
+        return sc.nextInt();
+    }
+
 }
