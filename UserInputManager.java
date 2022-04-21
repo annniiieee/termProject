33650768872d666ec05f05758a742c06874fc72f
Msrd1 <104034719+Msrd1@users.noreply.github.com>
2022-04-21 01:07:54 -0400

    Update UserInputManager.java

--- UserInputManager.java
+++ UserInputManager.java
@@ -11,7 +11,7 @@ package library;
  */
 public class UserInputManager {
     
-    public static int getuserId(){
+    public int getId(){
         Scanner sc = new Scanner(System.in);
         System.out.print("What is your id? " );
         int a = 0;
@@ -24,15 +24,66 @@ public class UserInputManager {
         return a;
     }
     
-    public static String getPassWord(){
+    public String getPassWord(){
         Scanner sc = new Scanner(System.in);
         System.out.print("Password: ");
         return sc.nextLine();        
     }
     
-    public static String getSearch(){
+    public String getSearch(){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your key word(s): ");
         return sc.nextLine();             
+    }
+    
+    public User createUser(){
+        String f = getFirstName();
+        String l = getLastName();
+        String a = getAddress();
+        return new User(User.getCurrentId(), createPassword(), l, f, a);
+    }
+    
+    public String getFirstName(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please input your first name.");
+        String fName = sc.nextLine();
+        while (fName.equals("")) {            
+            System.out.println("Empty name. Please input a name.");
+            fName = sc.nextLine();
+        }
+        return fName;
+    }
+
+    public String getLastName(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please input your last name.");
+        String lName = sc.nextLine();
+        while (lName.equals("")) {            
+            System.out.println("Empty name. Please input a name.");
+            lName = sc.nextLine();
+        }
+        return lName;
+    }
+    
+    public String getAddress(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please input your address.");
+        String address = sc.nextLine();
+        while (address.equals("")) {            
+            System.out.println("Empty address. Please input a address.");
+            address = sc.nextLine();
+        }
+        return address;  
+    }
+    
+    public String createPassword(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please input a password.");
+        String password = sc.nextLine();
+        while (password.equals("")) {            
+            System.out.println("Empty address. Please input a address.");
+            password = sc.nextLine();
+        }
+        return password;          
     }
 }
