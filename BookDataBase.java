57ac8513081eb601381e0f166715b088c8b22baa
annniiieee <100371639+annniiieee@users.noreply.github.com>
2022-04-22 15:19:46 -0400

    Update BookDataBase.java

--- BookDataBase.java
+++ BookDataBase.java
@@ -4,53 +4,50 @@ package termproject;
 import java.util.*;
 
 public class bookDatabase {
-    Map<Integer, String> linkedHashMap = new LinkedHashMap<Integer, String>();
-    String book; 
-    int key; 
-    public void addBook (int key, String book){
-        linkedHashMap.put(key, book);
+    Map<Integer, Book> linkedHashMap = new LinkedHashMap<>();
+    public void addBook (Book book){
+        linkedHashMap.put(book.bookId, book);
         
     }
 
-    public bookDatabase(String book, int key) {
-        this.book = book;
-        this.key = key;
+    public bookDatabase() {
+        Map<Integer, Book> linkedHashMap = new LinkedHashMap<>();        
     }
+
     
-    public void removeBook(int key){
+    
+    public void removeBook(int id){
         if (linkedHashMap.isEmpty()) {
             System.out.println("Empty book base");
             
         }
         else{
-            for (Map.Entry<Integer,String> entry : linkedHashMap.entrySet()) {
-                if (entry.getKey() == key) {
+            for (Map.Entry<Integer,Book> entry : linkedHashMap.entrySet()) {
+                if (entry.getKey() == id) {
                     linkedHashMap.remove(entry.getKey());
                 }
             }
         }
     }
     
-     public void removeBook(String book){
-        if (linkedHashMap.isEmpty()) {
-            System.out.println("Empty book base");
+    public boolean checkBookExistence(int id){
+        if (linkedHashMap.containsKey(id)) {
+            return true; 
             
         }
-        else{
-            for (Map.Entry<Integer,String> entry : linkedHashMap.entrySet()) {
-                if (entry.getValue() == book) {
-                    linkedHashMap.remove(entry.getValue());
-                }
-            }
+        else {
+            return false; 
         }
     }
     
+
     
-    
-    
-    
-    
-    
+    public void printBooks(){
+        System.out.println("The book list: ");
+        for (Map.Entry<Integer,Book> entry : linkedHashMap.entrySet()){
+            entry.getValue().printBook();
+        }
+    }
     
     
 }
